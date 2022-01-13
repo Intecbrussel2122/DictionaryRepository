005671c8ecb3bd8218bff6cb50ce834a53f21bb3
kenan kurda <kenankurda@hotmail.com>
2022-01-13 12:33:47 +0100

    test

--- ListRepository/Models/TelephoneManager.cs
+++ ListRepository/Models/TelephoneManager.cs
@@ -8,7 +8,7 @@ namespace ListRepository.Models
 {
     public class TelephoneManager : IRepository
     {
-
+        //list done right
         public void Delete(int id)
         {
             Data.ProductList.RemoveAt(id);
